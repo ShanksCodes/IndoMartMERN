a9dd0771eb3ba9a26be6ad9e22f5ee3f63a92622
ShanksCodes <99173788+ShanksCodes@users.noreply.github.com>
2022-12-06 20:48:01 +0530

    made few changes in schema

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const productSchema = mongoose.Schema({
+const productSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, "Please Enter product Name"],
@@ -47,11 +47,7 @@ const productSchema = mongoose.Schema({
   },
   reviews: [
     {
-      user: {
-        type: mongoose.Schema.ObjectId,
-        ref: "User",
-        required: true,
-      },
+    
       name: {
         type: String,
         required: true,
@@ -67,11 +63,7 @@ const productSchema = mongoose.Schema({
     },
   ],
 
-  user: {
-    type: mongoose.Schema.ObjectId,
-    ref: "User",
-    required: true,
-  },
+ 
   createdAt: {
     type: Date,
     default: Date.now,
