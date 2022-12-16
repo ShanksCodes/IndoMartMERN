068e70f2806a5393b00dfcdb4f3cf65087fbaef4
ShanksCodes <99173788+ShanksCodes@users.noreply.github.com>
2022-12-16 21:08:26 +0530

    added some error handling basic

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,15 @@ const express = require('express');
 
 const app = express();
 
+const errorMiddleware = require("./middleware/error");
+
 app.use(express.json())
 //route imports
 const product = require("./routes/productRoute");
 
 app.use("/api/v1",product);
+
+//Middleware for error
+
 
 module.exports = app
