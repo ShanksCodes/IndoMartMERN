5bc72d1a4128d36ae0bbd3b771bbd080a77b251f
ShanksCodes <99173788+ShanksCodes@users.noreply.github.com>
2022-12-25 20:33:56 +0530

    made some roles

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 
 const app = express();
 
+const cookieParser = require('cookie-parser');
 const errorMiddleware = require("./middleware/error");
 
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 //route imports
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
