cda4753e1a7b0c1140e827bb6324163248b5afc9
ShanksCodes <99173788+ShanksCodes@users.noreply.github.com>
2022-12-26 16:54:41 +0530

    reset pwd bkend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -7,6 +7,7 @@ const ApiFeatures = require("../utils/apifeatures");
 //Create product- admin route
 exports.createProduct = catchAsyncErrors(async (req,res,next)=>{
 
+   req.body.user = req.user.id;
    const product = await Product.create(req.body);
 
    res.status(201).json({
