2cd51fc64d3c59ca9fc23862753173a9ce7b9539
ShanksCodes <99173788+ShanksCodes@users.noreply.github.com>
2023-01-13 18:27:42 +0530

    nightmarish frontend work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,38 @@
 
 import './App.css';
-import Header from "./component/layout/Header.js";
-import { BrowserRouter as Router } from 'react-router-dom';
+import Header from "./component/layout/Header/Header.js";
+//import { BrowserRouter as Router,Route} from 'react-router-dom';
+//import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+//import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+
+
+import WebFont from "webfontloader";
+import React from 'react';
+import Footer from "./component/layout/Footer/Footer.js";
+import Home from "./component/Home/Home.js";
+
+
 
 function App() {
 
+  React.useEffect(()=>{
+    WebFont.load({
+      google:{
+        families:["Roboto","Droid Sans","Chilanka"]
+      }
+    })
+  },[]);
+
   return (
-    <Router>
+      <Router>
       <Header />
-    </Router>
+      <Routes>
+      <Route exact path="/" element={<Home/>} />
+      </Routes>
+      <Footer />
+      </Router>
+    
 
   );
   
