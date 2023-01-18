8d78b3951b13a07b9c0dd8af2b34cfaa4253de0e
ShanksCodes <99173788+ShanksCodes@users.noreply.github.com>
2023-01-18 20:54:39 +0530

    speeddial profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,16 @@ import Home from "./component/Home/Home.js";
 import Products from "./component/Product/Products.js";
 import Search from "./component/Product/Search.js";
 import LoginSignUp from "./component/User/LoginSignUp";
+import store from "./store";
+import { loadUser } from "./actions/userAction";
+import UserOptions from "./component/layout/Header/UserOptions.js";
+import { useSelector } from "react-redux";
+import Profile from "./component/User/Profile.js";
+import ProtectedRoute from "./component/Route/ProtectedRoute";
+
+
+
+
 
 
 
@@ -21,20 +31,24 @@ import LoginSignUp from "./component/User/LoginSignUp";
 
 function App() {
 
+  const { isAuthenticated, user } = useSelector((state) => state.user);
+
   React.useEffect(()=>{   
     WebFont.load({
       google:{
         families:["Roboto","Droid Sans","Chilanka"]
       },
     });
+    store.dispatch(loadUser());
   },[]);
 
   return (
       <Router>
       <Header />
+      {isAuthenticated && <UserOptions user={user} />}
       <Switch>
 
-
+   
       <Route exact path="/" component={Home} />
       <Route exact path="/product/:id" component={ProductDetails} />
       <Route exact path="/products" component={Products} />
@@ -42,6 +56,8 @@ function App() {
     
 
       <Route exact path="/search" component={Search} />
+      <ProtectedRoute exact path="/account" component={Profile} />
+
       <Route exact path="/login" component={LoginSignUp} />
 
 
