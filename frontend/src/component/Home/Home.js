4493de8c061834471274c2908e768d625e65dac6
ShanksCodes <99173788+ShanksCodes@users.noreply.github.com>
2023-01-23 16:34:19 +0530

    Added reviews module

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,26 +1,17 @@
-import React, { Fragment, useEffect } from 'react';
-import {CgMouse} from "react-icons/cg";
+import React, { Fragment, useEffect } from "react";
+import { CgMouse } from "react-icons/all";
 import "./Home.css";
-import Product from "./ProductCard.js";
-import MetaData from '../layout/MetaData';
-import { clearErrors, getProduct } from '../../actions/productAction';
-import {useSelector,useDispatch} from "react-redux";
-import Loader from '../layout/Loader/Loader';
-import {useAlert} from "react-alert";
+import ProductCard from "./ProductCard.js";
+import MetaData from "../layout/MetaData";
+import { clearErrors, getProduct } from "../../actions/productAction";
+import { useSelector, useDispatch } from "react-redux";
+import Loader from "../layout/Loader/Loader";
+import { useAlert } from "react-alert";
 
-
-/*const product= {
-  name:"Blue Tshirt",
-  images: [{url: "https://i.ibb.co/DRST11n/1.webp"}],
-  price: "Rs3000",
-  _id: "abhishek",
-}*/
-
-const Home=()=>{
-  const alert= useAlert();
-  const dispatch= useDispatch();
-  const {loading,error,products}= useSelector((state)=>state.products);
-
+const Home = () => {
+  const alert = useAlert();
+  const dispatch = useDispatch();
+  const { loading, error, products } = useSelector((state) => state.products);
 
   useEffect(() => {
     if (error) {
@@ -28,14 +19,12 @@ const Home=()=>{
       dispatch(clearErrors());
     }
     dispatch(getProduct());
-  }, [dispatch, error,alert]);
+  }, [dispatch, error, alert]);
 
-
-    return(
-
-      <Fragment>
+  return (
+    <Fragment>
       {loading ? (
-       <Loader />
+        <Loader />
       ) : (
         <Fragment>
           <MetaData title="ECOMMERCE" />
@@ -56,15 +45,13 @@ const Home=()=>{
           <div className="container" id="container">
             {products &&
               products.map((product) => (
-                <Product product={product} />
+                <ProductCard key={product._id} product={product} />
               ))}
           </div>
         </Fragment>
       )}
     </Fragment>
-
-    );
+  );
 };
-
 
 export default Home;
